6bfc92bc4b71a38f231836f825261630d5fa1937
Chandrika-Atthunuru <chandrikaatthunuru99@gmail.com>
2024-04-16 21:23:29 +0530

    login and student model created

---
+++ src/app/models/student.ts
@@ -0,0 +1,26 @@
+export interface Student {
+name:string;
+gender: string;
+mobile: string;
+email: string;
+batch: string;
+address: {
+city: string;
+mandal: string;
+district: string;
+state: string;
+pincode: string;
+},
+education: [
+{qualification:string,year: number, percentage: number},
+],
+company: {
+name: string;
+location: string;
+package: string;
+offerDate: string;
+},
+sourceType: string;
+sourceFrom: string;
+referralName: string;
+}

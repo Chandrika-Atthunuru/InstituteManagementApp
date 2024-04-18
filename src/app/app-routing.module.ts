c9209a60c991147f362b9242fd2a7efa047df351
Chandrika-Atthunuru <chandrikaatthunuru99@gmail.com>
2024-04-18 13:03:05 +0530

    Dashboard implementation completed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,10 +4,16 @@ import { LoginComponent } from './components/login/login.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { FallbackComponent } from './components/fallback/fallback.component';
 import { AuthGuard } from './guards/auth.guard';
+import { CreateStudentComponent } from './components/create-student/create-student.component';
+import { AllStudentsComponent } from './components/all-students/all-students.component';
 
 const routes: Routes = [
   {path:'login',component:LoginComponent},
-  {path:'dashboard',component:DashboardComponent,canActivate:[AuthGuard]},
+  {path:'dashboard',component:DashboardComponent,canActivate:[AuthGuard],children:[
+    {path:'create-student',component:CreateStudentComponent},
+    {path:'all-students',component:AllStudentsComponent}
+  ]
+  },
   {path:'',component:LoginComponent},
   {path:'**',component:FallbackComponent}
 ];

2a8705bbad9db99b4b6bebd5cd58a6b54ee91c98
Chandrika-Atthunuru <chandrikaatthunuru99@gmail.com>
2024-04-18 11:42:20 +0530

    Token storage completed

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, RequiredValidator, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { Token } from 'src/app/models/token';
 import { LoginService } from 'src/app/services/login.service';
 
@@ -13,10 +14,12 @@ export class LoginComponent {
   email:new FormControl(null,[Validators.required,Validators.email]),
   password:new FormControl(null,[Validators.required ,Validators.minLength(6),Validators.maxLength(10),Validators.pattern(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*])(?=.*[a-zA-Z]).{8,}$/)])
  })
- constructor(private _loginService:LoginService){}
+ constructor(private _loginService:LoginService, private _router:Router){}
  login(){
   this._loginService.postlogindata(this.loginform.value).subscribe(
     (data:Token)=>{
+     localStorage.setItem("token",data.token);
+     this._router.navigateByUrl("/dashboard")
      alert("Login successfull")
     },
     (err:Token)=>{

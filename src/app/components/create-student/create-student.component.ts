2bc7b3e90d9e31bd6c5bad10e18ce3a1a3c5fcc3
Chandrika-Atthunuru <chandrikaatthunuru99@gmail.com>
2024-04-19 15:55:10 +0530

    Form validations completed

--- src/app/components/create-student/create-student.component.ts
+++ src/app/components/create-student/create-student.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormArray, FormControl, FormGroup } from '@angular/forms';
+import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 import { Student } from 'src/app/models/student';
 import { Token } from 'src/app/models/token';
 
@@ -10,36 +10,36 @@ import { Token } from 'src/app/models/token';
 })
 export class CreateStudentComponent {
 public studentform:FormGroup=new FormGroup({
-  name:new FormControl(),
-  gender:new FormControl(),
-  mobile:new FormControl(),
-  email:new FormControl(),
-  batch:new FormControl(),
+  name:new FormControl(null,[Validators.required,Validators.minLength(3)]),
+  gender:new FormControl(null,[Validators.required]),
+  mobile:new FormControl(null,[Validators.required,Validators.min(1000000000),Validators.max(9999999999)]),
+  email:new FormControl(null,[Validators.required,Validators.email]),
+  batch:new FormControl(null,[Validators.required]),
   address:new FormGroup({
-    city:new FormControl(),
-    mandal:new FormControl(),
-    district:new FormControl(),
-    state:new FormControl(),
-    pincode:new FormControl(),
+    city:new FormControl(null,[Validators.required]),
+    mandal:new FormControl(null,[Validators.required]),
+    district:new FormControl(null,[Validators.required]),
+    state:new FormControl(null,[Validators.required]),
+    pincode:new FormControl(null,[Validators.required,Validators.min(100000),Validators.max(999999)]),
   }),
   education:new FormArray([]),
   company:new FormGroup({
-    name:new FormControl(),
-    location:new FormControl(),
-    package:new FormControl(),
-    offerDate:new FormControl()
+    name:new FormControl(null,[Validators.required]),
+    location:new FormControl(null,[Validators.required]),
+    package:new FormControl(null,[Validators.required]),
+    offerDate:new FormControl(null,[Validators.required])
   }),
-  sourceType:new FormControl(),
+  sourceType:new FormControl(null,[Validators.required]),
 })
 constructor(){
   this.studentform.get('sourceType')?.valueChanges.subscribe(
     (data:any)=>{
       if(data=='direct'){
-        this.studentform.addControl("sourceForm",new FormControl())
+        this.studentform.addControl("sourceForm",new FormControl(null,[Validators.required]))
         this.studentform.removeControl("referralName")
       }
       else{
-        this.studentform.addControl("referralName",new FormControl())
+        this.studentform.addControl("referralName",new FormControl(null,[Validators.required]))
         this.studentform.removeControl("sourceForm")
       }
     }
@@ -51,9 +51,9 @@ get educationFormArray(){
  add(){
    this.educationFormArray.push(
     new FormGroup({
-      qualification: new FormControl(),
-      year: new FormControl(),
-      percentage: new FormControl(),
+      qualification: new FormControl(null,[Validators.required]),
+      year: new FormControl(null,[Validators.required]),
+      percentage: new FormControl(null,[Validators.required]),
     })
    )
  }
@@ -62,6 +62,6 @@ delete(i:number){
 }
 
 submit(){
-
+this.studentform.markAllAsTouched();
 }
 }
